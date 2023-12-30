403ce39b7e274f8e296d85a8eb46330327c646ce
PritamTharu <tharupritam100@gmail.com>
2023-12-31 02:21:08 +0530

    Added new files

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const name = "pritam"
+
+let age = 24
+
+var address = "ahmedabad"
+
+phone =  9936000000
+
+let id;
+
+var address = 'butwal'
+
+
+
+console.table([name,age,address,phone,id])
---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //It treat all js code as a newer version
+
+// alert("Helo")
+
+int
+BigInt
+String
+Boolean
+null       // datatypes of null is object
+undefined
+Object
+Symbol
+
+
